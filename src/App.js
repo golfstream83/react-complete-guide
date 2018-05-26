2871349de5dd11b4e365d071ed2573fcb6f9af4c
Тулин Виктор Владимирович <VVTulin.OUT@sberbank.ru>
2018-05-26 11:33:12 +0300

    Added component Person with JSX code. Added state to component App.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
           <h1 className="App-title">Welcome to React</h1>
+          <button>Switch Name</button>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
       </div>
     );
   }
