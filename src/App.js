481ff774ef0f183125f508b982a5dbc9ce30f684
Тулин Виктор Владимирович <VVTulin.OUT@sberbank.ru>
2018-05-30 21:11:08 +0300

    Enabled using CSS modules. Added pseudo selectors.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
-import Radium, {StyleRoot} from 'radium';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -42,20 +41,8 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    };
-
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -71,36 +58,30 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
-      <StyleRoot>
-        <div className="App">
-            <h1 className="App-title">Hi, I'm a React App</h1>
-            <p className={classes.join(' ')}>This is really working!</p>
-            <button
-              style={style}
-              onClick={this.togglePersonsHandler}>
-              Toggle Persons
-            </button>
-            {persons}
-        </div>
-      </StyleRoot>
+      <div className={classes.App}>
+          <h1 className="App-title">Hi, I'm a React App</h1>
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
+          <button
+            className={btnClass}
+            onClick={this.togglePersonsHandler}>
+            Toggle Persons
+          </button>
+          {persons}
+      </div>
     );
   }
 }
 
-export default Radium(App);
+export default App;
