6c6283910fa01130c7642587b2fa136d9b4b6439
Тулин Виктор Владимирович <VVTulin.OUT@sberbank.ru>
2018-06-02 20:44:43 +0300

    Added props for the title

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -17,7 +17,7 @@ const cockpit = (props) => {
 
     return (
         <div className={classes.Cockpit}>
-            <h1 className="App-title">Hi, I'm a React App</h1>
+            <h1 className="App-title">{props.appTitle}</h1>
             <p className={assignedClasses.join(' ')}>This is really working!</p>
             <button
                 className={btnClass}
